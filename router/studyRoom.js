84a4fce7903bb7fe2c4a82eff18819620fc03784
flypig-hub <103496007+flypig-hub@users.noreply.github.com>
2022-07-13 14:27:33 +0900

    studyroom-2

--- router/studyRoom.js
+++ router/studyRoom.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const studyRoomCtl = require("../controller/studyRoom");
 
-const authorization = require("../middlewares/auth-middleware");
+// const authorization = require("../middlewares/auth-middlewares");
 
 router.get("/list/all", studyRoomCtl.allRoomList);
 router.get("/list/keyword/:roomPurpose", studyRoomCtl.keywordList);
-router.post("/hostRoom", authorization, studyRoomCtl.createRoom);
-router.post("/enterRoom/:roomId", authorization, studyRoomCtl.enterRoom);
-router.delete("/exitRoom/:roomId", authorization, studyRoomCtl.exitRoom);
-router.post("/checkRoomPw/:roomId", authorization, studyRoomCtl.checkRoomPw);
+router.post("/hostRoom",  studyRoomCtl.createRoom);
+router.post("/enterRoom/:roomId",  studyRoomCtl.enterRoom);
+router.delete("/exitRoom/:roomId",  studyRoomCtl.exitRoom);
+router.post("/checkRoomPw/:roomId",  studyRoomCtl.checkRoomPw);
 
 module.exports = router;
